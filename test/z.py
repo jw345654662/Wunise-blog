e8242ea6e2df39ab8879a5f6ee2624be6404a7ef
wangyuzhi <429373960@qq.com>
2016-03-31 12:01:06 +0000

    Delete z.py

--- test/z.py
+++
@@ -1,8 +0,0 @@
-# -- coding: utf-8 --
-import time
-#获得当前时间时间戳
-now = int(time.time())  #这是时间戳
-#转换为其他日期格式,如:"%Y-%m-%d %H:%M:%S"
-timeArray = time.localtime(now)
-otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
-print  otherStyleTime
